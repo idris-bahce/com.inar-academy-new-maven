1a363ba3ddb624136b0697b4b006df0fd4852f0a
idris <idrisbahce26@gmail.com>
2023-01-30 09:14:11 +0300

    exc

--- RestAssured/src/test/java/Files/JiraTest.java
+++ RestAssured/src/test/java/Files/JiraTest.java
@@ -2,6 +2,10 @@ package Files;
 
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -9,6 +13,7 @@ public class JiraTest {
     public static void main(String[] args) {
         RestAssured.baseURI = "http://localhost:8080";
 
+        //Login Scenario
         SessionFilter session = new SessionFilter();
 
         String response = given().header("Content-Type", "application/json").body("{" +
@@ -17,13 +22,42 @@ public class JiraTest {
                 .then().extract().response().asString();
 
 
-        given().pathParam("id", "10004").log().all().header("Content-Type", "application/json").body("{\n" +
-                "    \"body\": \"Hey I am updating the existing comment.\",\n" +
+        String expectedMessage="Hi How are you?";
+        //Add comment
+        String addCommentResponse = given().pathParam("id", "10004").log().all().
+                header("Content-Type", "application/json").body("{\n" +
+                "    \"body\": \""+expectedMessage+"\",\n" +
                 "    \"visibility\": {\n" +
                 "        \"type\": \"role\",\n" +
                 "        \"value\": \"Administrators\"\n" +
                 "    }\n" +
                 "}").filter(session).when().post("/rest/api/2/issue/{id}/comment").then().assertThat()
-                .statusCode(201);
+                .statusCode(201).extract().response().asString();
+        JsonPath js = new JsonPath(addCommentResponse);
+        String commentID = js.getString("id");
+
+
+        //Add attachment
+        given().header("X-Atlassian-Token","no-check").filter(session).pathParam("id", "10004")
+                .queryParam("fields","comment").header("Content-Type","multipart/form-data")
+                .multiPart("file",new File("jira.txt")).when().
+                post("rest/api/2/issue/{id}/attachments").then().log().all().assertThat().statusCode(200)
+                ;
+
+        //Get Issue
+        String issueDetails = given().filter(session).pathParam("id", "10004").log().all().when().
+                get("/rest/api/2/issue/{id}").then().log().all().extract().response().asString();
+
+        System.out.println(issueDetails);
+        JsonPath js2 = new JsonPath(issueDetails);
+        int commentsCounts = js2.get("fields.comment.comments.size()");
+        for (int i = 0; i < commentsCounts; i++) {
+            String commentIDIssue = js2.get("fields.comment.comments["+i+"].id").toString();
+            if (commentIDIssue.equalsIgnoreCase(commentID)){
+                String message = js2.get("fields.comment.comments["+i+"].body").toString();
+                System.out.println(message);
+                Assert.assertEquals(message,expectedMessage);
+            }
+        }
     }
 }
